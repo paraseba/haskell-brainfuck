2db336a61a57121b53a34d227753979035f27f30
Sebastián Bernardo Galkin <paraseba@gmail.com>
2014-08-01 23:42:24 -0700

    better imports

--- src/Eval.hs
+++ src/Eval.hs
@@ -1,6 +1,6 @@
 module Eval (
   eval, evalBS, evalStr
-  ,Machine
+  ,Machine(Machine)
   ,tape, putByte, getByte
   ,defaultIOMachine
   ,simulator, SimState (SimState), simStateOutput
@@ -9,17 +9,16 @@ module Eval (
 
 import Data.Int (Int8)
 import Data.List (foldl')
-import Control.Monad.State
+import Control.Monad.State (State, modify, state)
 import qualified Data.ByteString.Lazy as BS
 import qualified Data.ByteString.Lazy.Char8 as BSC
-import Debug.Trace
 
-import Parser
-import Tape
+import qualified Parser as P
+import qualified Tape   as T
 
 type Putter m = Int8 -> m ()
 type Getter m = m Int8
-type BFTape = Tape Int8
+type BFTape = T.Tape Int8
 
 data Machine m = Machine { tape :: BFTape
                           ,putByte :: Putter m
@@ -29,46 +28,46 @@ data Machine m = Machine { tape :: BFTape
 update :: (BFTape -> BFTape) -> Machine m -> Machine m
 update f m = m{tape = f $ tape m}
 
-eval :: Monad m => Machine m -> Program -> m (Machine m)
+eval :: Monad m => Machine m -> P.Program -> m (Machine m)
 eval machine program =
   foldl' (flip evalOp) (return machine) program
 
 evalBS :: Monad m => Machine m -> BS.ByteString -> m (Machine m)
 evalBS machine program =
-  case fmap (eval machine) $ parseProgram program of
+  case fmap (eval machine) $ P.parseProgram program of
     -- fixme
     (Right res) -> res
 
 evalStr :: Monad m => Machine m -> String -> m (Machine m)
 evalStr m = evalBS m . BSC.pack
 
-evalOp :: Monad m => Op -> m (Machine m) -> m (Machine m)
+evalOp :: Monad m => P.Op -> m (Machine m) -> m (Machine m)
 
-evalOp IncP machine = machine >>= return . update right --move the head right
-evalOp DecP machine = machine >>= return . update left
-evalOp Inc  machine = machine >>= return . update inc
-evalOp Dec  machine = machine >>= return . update dec
+evalOp P.IncP machine = machine >>= return . update T.right --move the head right
+evalOp P.DecP machine = machine >>= return . update T.left
+evalOp P.Inc  machine = machine >>= return . update T.inc
+evalOp P.Dec  machine = machine >>= return . update T.dec
 
-evalOp PutByte machine = do
+evalOp P.PutByte machine = do
   m <- machine
-  let current = (rTape . tape) m
+  let current = (T.rTape . tape) m
   putByte m current
   return m
 
-evalOp GetByte machine = do
+evalOp P.GetByte machine = do
   m <- machine
   b <- getByte m
-  (return . update (wTape b)) m
+  (return . update (T.wTape b)) m
 
-evalOp (Loop ops) machine = do
+evalOp (P.Loop ops) machine = do
   m <- machine
-  if ((==0) . rTape . tape) m
+  if ((==0) . T.rTape . tape) m
     then return m
-    else evalOp (Loop ops) $ eval m ops
+    else evalOp (P.Loop ops) $ eval m ops
 
 
 defaultIOMachine :: Machine IO
-defaultIOMachine = Machine blankTape
+defaultIOMachine = Machine T.blankTape
                            (putChar . toEnum . fromIntegral)
                            (fmap (fromIntegral . fromEnum) getChar)
 
@@ -82,7 +81,7 @@ emptyState :: Int8 -> SimState
 emptyState inputByte = SimState [] []
 
 simulator :: Machine (State SimState)
-simulator = Machine blankTape
+simulator = Machine T.blankTape
                     (\byte -> modify (writeByte byte))
                     (state readByte)
             where  writeByte byte s@(SimState{output = o}) = s{output = byte : o}
