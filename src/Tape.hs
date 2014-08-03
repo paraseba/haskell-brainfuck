a2d02bea5d75bc09a29faa5fb0aee9e12e87c78f
Sebastián Bernardo Galkin <paraseba@gmail.com>
2014-08-03 02:26:49 -0700

    haddock documentation for Tape

--- src/Tape.hs
+++ src/Tape.hs
@@ -1,3 +1,16 @@
+{-|
+Module      : Tape
+Description : Implement the brainfuck tape
+Copyright   : (c) Sebastian Galkin, 2014
+License     : MIT
+Maintainer  : paraseba@gmail.com
+Stability   : experimental
+
+Provides a type and operations to implement the brainfuck tape. The tape has
+the concept of a pointer, and the pointer can be incremented or decremented.
+
+-}
+
 module Tape (
   Tape(Tape)
   ,rTape, wTape
@@ -8,34 +21,60 @@ module Tape (
 
 import Data.Int (Int8)
 
--- Tape left-tape current right-tape
-data Tape t = Tape [t] t [t] deriving (Show)
+-- | Brainfuck tape. Constructor arguments correspond to
+--
+--   1. left of the current pointer
+--   2. current pointed value
+--   3. right of the current pointer
+--
+-- The left part of the tape is reversed, so the first element of the list
+-- is the rightmost position. The right list is normal order, its first element
+-- is the leftmost one.
+data Tape t = Tape [t] t [t]
+  deriving (Show)
 
-wTape :: t -> Tape t -> Tape t
+-- | Write element to the current position in the tape
+wTape :: t      -- ^ The element to write
+      -> Tape t -- ^ The tape
+      -> Tape t -- ^ The modified tape
 wTape b (Tape l _ r) = Tape l b r
 
-rTape :: Tape t -> t
+-- | Read the pointed element
+rTape :: Tape t -- ^ The tape
+      -> t      -- ^ The element currently pointed by the pointer
 rTape (Tape _ current _) = current
 
 update :: (t -> t) -> Tape t -> Tape t
 update f t = wTape (f $ rTape t) t
 
-inc :: Num a => Tape a -> Tape a
+-- | Increment the currently pointed element
+inc :: Num a
+    => Tape a -- ^ The tape
+    -> Tape a -- ^ The tape with the current position incremented
 inc = update (+1)
 
-dec :: Num a => Tape a -> Tape a
+-- | Decrement the currently pointed element
+dec :: Num a
+    => Tape a -- ^ The tape
+    -> Tape a -- ^ The tape with the current position decremented
 dec = update (+(-1))
 
--- Move the head right
-right :: Tape a -> Tape a
+-- | Move the pinter to the right
+right :: Tape a -- ^ The tape
+      -> Tape a -- ^ The pointer in the resulting tape points to the element
+                -- to the right of the pointer in the original tape
 right (Tape l c (r:rs)) = Tape (c:l) r rs
 
--- Move the head left
-left :: Tape a -> Tape a
+-- | Move the pinter to the left
+left :: Tape a -- ^ The tape
+     -> Tape a -- ^ The pointer in the resulting tape points to the element
+               -- to the left of the pointer in the original tape
 left (Tape (l:ls) c r) = Tape ls l (c:r)
 
 constTape :: t -> Tape t
 constTape b = Tape [] b (repeat b)
 
+-- | A @(0 :: 'Int8')@ initialized, infinite 'Tape' pointing to its leftmost position.
+-- An attemp to move the pointer left will result in an error
 blankTape :: Tape Int8
 blankTape = constTape 0
