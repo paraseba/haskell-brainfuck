d5fb41342111c2150727fd07b31dcc8772916c45
Sebastián Bernardo Galkin <paraseba@gmail.com>
2014-08-03 01:55:43 -0700

    haddock documentation for Parser

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,3 +1,15 @@
+{-# OPTIONS_HADDOCK show-extensions #-}
+
+{-|
+Module      : Parser
+Description : Parse brainfuck program into a Program abstract datatype
+Copyright   : (c) Sebastian Galkin, 2014
+License     : MIT
+Maintainer  : paraseba@gmail.com
+Stability   : experimental
+
+-}
+
 module Parser (
        Program
      , Op(..)
@@ -12,9 +24,18 @@ import Text.Parsec.Char                  (space, spaces, char)
 import Control.Applicative               ((<*))
 import qualified Data.ByteString.Lazy as BS
 
-data Op = IncP | DecP | Inc | Dec | PutByte | GetByte | Loop [Op]
+-- | Brainfuck operations
+data Op
+  = IncP      -- ^ Increment the byte pointer: ">"
+  | DecP      -- ^ Decrement the byte pointer: "<"
+  | Inc       -- ^ Increment the byte pointed by the pointer: "+"
+  | Dec       -- ^ Decrement the byte pointed by the pointer: "-"
+  | PutByte   -- ^ Write the byte pointed by the pointer (side-effect): "."
+  | GetByte   -- ^ Read a byte, write at the current location (side-effect): ","
+  | Loop [Op] -- ^ Loop over 'Op's untill current == 0: "[...]"
   deriving (Show,Eq)
 
+-- | A program is a list of 'Op's. One possible 'Op' is a 'Loop' of other 'Op's
 type Program = [Op]
 
 program :: Parser Program
@@ -44,5 +65,6 @@ simpleOp = fmap build simpleChar
 loop :: Parser Op
 loop = fmap Loop $ between (char '[') (char ']') program
 
+-- | Parse program stream. Returns an error or the parsed 'Program'
 parseProgram :: BS.ByteString -> Either ParseError Program
 parseProgram s = runP fullProgram () "" s
